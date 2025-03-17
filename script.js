187642990ff1dc3a420822aee35ddb475a84c247
Saurabh Mishra <84716420+Saurabhmishra1909@users.noreply.github.com>
2025-03-17 19:58:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,55 +1,28 @@
 function convertToRoman(num) {
-  	const obj = {
-      0:['M',1000], 
-      1:['D', 500], 
-      2:['C', 100], 
-      3:['L', 50], 
-      4:['X', 10], 
-      5:['V', 5], 
-      6:['I', 1]
+    const obj = {
+        0: ['M', 1000], 
+        1: ['CM', 900], 
+        2: ['D', 500], 
+        3: ['CD', 400], 
+        4: ['C', 100], 
+        5: ['XC', 90], 
+        6: ['L', 50], 
+        7: ['XL', 40], 
+        8: ['X', 10], 
+        9: ['IX', 9], 
+        10: ['V', 5], 
+        11: ['IV', 4], 
+        12: ['I', 1]
     };
-
-  //your code here
-   let result = '';
-
-  // Handle subtractive combinations manually
-  if (num >= 900) {
-    result += 'CM';
-    num -= 900;
-  }
-  if (num >= 400) {
-    result += 'CD';
-    num -= 400;
-  }
-  if (num >= 90) {
-    result += 'XC';
-    num -= 90;
-  }
-  if (num >= 40) {
-    result += 'XL';
-    num -= 40;
-  }
-  if (num >= 9) {
-    result += 'IX';
-    num -= 9;
-  }
-  if (num >= 4) {
-    result += 'IV';
-    num -= 4;
-  }
-
-  // Iterate through the obj mapping
-  for (const key in obj) {
-    const [symbol, value] = obj[key];
-    while (num >= value) {
-      result += symbol;
-      num -= value;
+    let result = '';
+    for (let i in obj) {
+        let [symbol, value] = obj[i];
+        while (num >= value) {
+            result += symbol;
+            num -= value;
+        }
     }
-  }
-
-  return result;
-}	
-
+    return result;
 }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
